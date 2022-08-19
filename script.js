3795342dac56bc4faead7f27dea34018272e32e3
MasterZen7 <adrian.saban7@gmail.com>
2022-08-19 16:01:38 +0200

    Added game logic

--- script.js
+++ script.js
@@ -8,5 +8,25 @@ const computerChoice = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-
+    if (playerSelection === computerSelection) {
+        return `Its a tie!. You both picked ${playerSelection}`
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore ++
+        return `You lose!. Paper covers rock`
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore ++
+        return `You win!. Rock crushes scissors`
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore ++
+        return `You lose!. Scissors cut paper`
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore ++
+        return `You win!. Paper covers rock`
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore ++
+        return `You lose!. Rock crushes scissors`
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore ++
+        return `You win!. Scissors cut paper`
+    }
 }
